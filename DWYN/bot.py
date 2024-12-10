dd3216f2f4eec69c9edc8d717dfad23d07c025c6
Amilie <amilie@allthingslinux.com>
2024-12-10 18:01:23 +0000

    :guh: it's broken :cri:

--- DWYN/bot.py
+++ DWYN/bot.py
@@ -1,8 +1,15 @@
 import discord
 from discord.ext import commands
+
 import os
 from dotenv import load_dotenv
 import asyncio
+
+#import logging
+
+#logging.basicConfig(level=logging.DEBUG,  # Minimum level to log
+#                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',  # Log message format
+#                    handlers=[logging.StreamHandler()])  # Log to console
 
 # Prepares intents for usage, please review your bot dashboard if you are hosting your own service.
 intents = discord.Intents.default()
@@ -11,7 +18,7 @@ intents.members = True
 intents.presences = True
 
 # Initialising command prefix and the bot's intents.
-client = commands.Bot(command_prefix=">", intents=intents)
+client = commands.Bot(command_prefix=">", intents=intents, application_id=1315778951842500749)
 
 # Ensuring client has successfully logged in.
 @client.event
@@ -29,10 +36,12 @@ async def main():
     # Specify your cogs here!
     try:
         await client.load_extension('cogs.utility.ping')
-        await client.load_extension('cogs.utility.joinlogs')
-        print(f"Successfully loaded all cogs.")
+        await client.load_extension('cogs.utility.join-logs')
+        await client.load_extension('cogs.botutils.sync')
+        await client.tree.sync()
+        print("Successfully loaded all cogs.")
     except Exception as e:
-        print(f"One or more cogs failed loading.")
+        print(f"One or more cogs failed loading: {e}")
 
     # Loads bot token.
     load_dotenv()
